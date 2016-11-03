09e0430a6d329f2e2c76964477f5ce3386d80f97
Júlio Dutra <dutra.julio@gmail.com>
2016-11-03 20:55:41 -0200

    Implementando leitura de lista de urls declaradas no arquivo urls.txt. Nesse arquivo é necessário colocar ao final da url o nome do coletor afim de facilitar a identificação dos arquivos de saída.
    O script agora identifica os campos da url e via api verifica o id da página de origem do post. Após isso ele realiza a coleta dos comentários do post e salva no arquivo de saída.

--- coleta.pl
+++ coleta.pl
@@ -8,29 +8,57 @@ use JSON qw( decode_json );
 use Path::Class;
 
 #Preenchimento obrigatório
-my $token = "EAACEdEose0cBAFpx8o4QHzab7CB5Yd4elvjvhPZA0D4SBAqSg3lItv5zuKOUZA5G4MlP4l8Bk1eq6gjZAbZCgFN1OAyQoXRSQJzFXyQVZAU3W76xHihr7DTxdimyvMQCTKXhYmvPhyVUtDmhqRsP4vTEk0au6Pk8POyEFjyCeqgZDZD";
-my $pageid = "303522857815";
-my @postids = ("10154022172737816", "10154021857427816", "10154021505512816");
+my $token = "EAACEdEose0cBAHrA64BbcVRfKGPfERRo4lgaXrACLmzHJe4aSXouQCT85dYdbanCFStKuvfegrNRwf41vBiQLatXEbz81ifuitvi0bGQdNJPA1QLModOHsZCPh6ZCQUAEQIw8byvxcsmNZB3xqZChwjS2UqYW0tEeVaLQMVZA5QZDZD";
 
 my $site = "https://graph.facebook.com/";
-my $postid;
+
 my $dir = dir("coletas");
 
-foreach $postid (@postids){
+my $filename = "urls.txt";
+open ( my $fh, '<', $filename)
+  or die "Não consegui abrir $filename!\n";
+
+while (my $row = <$fh>) {
   my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
   my $date = "$year$mon$wday$hour$min$sec";
-  my $post = "$pageid"."_"."$postid";
-  my $params = "v2.8/$post/comments?fields=message&filter=stream&access_token=$token";
-  # my $params = "v2.8/$post/comments?fields=message&filter=stream&limit=583&access_token=$token";
-  # summary=true&fields=total_count&filter=stream
-  my $url = $site.$params;
 
+  chomp $row;
+  my @fields = split(/\//, $row);
+  my $page = $fields[3];
+  my $postid = $fields[5];
+  my $coletor = $fields[6];
+
+  my $params = "v2.8/$page?access_token=$token";
+  my $url = $site.$params;
   my $curl = WWW::Curl::Easy->new;
 
-  my $filename = "coletas/coleta_$postid"."_"."$date.txt";
+  $curl->setopt(CURLOPT_HEADER,0);
+  $curl->setopt(CURLOPT_URL, $url);
+  $curl->setopt(CURLOPT_WRITEDATA, \my $response_body);
+
+  my $retcode = $curl->perform;
+  my $decoded = decode_json($response_body);
+
+  my $pageid;
+  if ( $retcode == 0){
+    $pageid = $decoded->{'id'};
+  } else {
+    print ("Merda, erro: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
+  }
+
+  my $post = "$pageid"."_"."$postid";
+  $params = "v2.8/$post/comments?fields=message&filter=stream&access_token=$token";
+  # my $params = "v2.8/$post/comments?fields=message&filter=stream&limit=583&access_token=$token";
+  # summary=true&fields=total_count&filter=stream
+
+  $url = $site.$params;
+  $curl = WWW::Curl::Easy->new;
+
+  my $filename = "coletas/coleta_$postid"."_"."$date"."_"."$coletor".".txt";
   open my $fh, '>:encoding(UTF-8)', $filename;
 
   my $i = 1;
+  my $q = 1;
 
   my $total = 0;
 
@@ -56,14 +84,15 @@ foreach $postid (@postids){
       print $qtd." comentários na página $i\n";
 
       foreach my $comment ( @comments ){
-        print $fh $comment->{'message'}." ";
+        # $comment->{'message'} =~ s/[^[:ascii:]]//g;
+        print $fh $comment->{'message'}."\r\n";
       }
       if ( $decoded->{'paging'}{'next'}){
         $url = $decoded->{'paging'}{'next'};
         $i++;
       } else {
         print $total." comentários coletados.\n";
-        print "Fim da coleta!\n";
+        print "Fim da coleta!\n\n";
         close $fh;
         last;
       }
