10a88841257bd84a5aa7c72c6f184eed3bdab814
Júlio Dutra <dutra.julio@gmail.com>
2016-11-03 17:10:24 -0200

    Melhorias, status e comentários.

--- coleta.pl
+++ coleta.pl
@@ -1,4 +1,4 @@
-#!/bin/perl
+#!/bin/perl -w
 
 use strict;
 use warnings;
@@ -7,21 +7,30 @@ use XML::LibXML;
 use JSON qw( decode_json );
 use Path::Class;
 
-my $token = "EAACEdEose0cBANBe26C7LUob0HFTXkv06k6sNbLcr8sB8MlJvOvHtrHE310Tn1Ljd22ZAXKl2RLNz49QyZAS5kuXzCfgVVcqVaM7NlLoMf3fCOqRbI3vVVBtCUOaKPmZBy4mPPEaqJUjNbuXT2rdGXPgW2SWJXb2ios0moJjwZDZD";
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+my $date = "$year$mon$wday$hour$min$sec";
+
+my $token = "1820225528248588|v8dvIGo5mmcAawIWsjdruqnOqxs";
 my $site = "https://graph.facebook.com/";
 my $pageid = "5550296508";
-my $postid = "10154847775816509";
+my $postid = "10155489801406509";
 my $post = "$pageid"."_"."$postid";
-# my $params = "v2.8/$post/comments?fields=message&filter=stream&access_token=$token";
-my $params = "v2.8/$post/comments?fields=message&filter=stream&limit=583&access_token=$token";
+my $params = "v2.8/$post/comments?fields=message&filter=stream&access_token=$token";
+# my $params = "v2.8/$post/comments?fields=message&filter=stream&limit=583&access_token=$token";
 # summary=true&fields=total_count&filter=stream
 my $url = $site.$params;
 
 my $curl = WWW::Curl::Easy->new;
 
 my $dir = dir("coletas");
-my $file = $dir->file("coleta_$postid.txt");
-my $file_handle = $file->openw();
+my $filename = "coletas/coleta_$postid"."_"."$date.txt";
+open my $fh, '>:encoding(UTF-8)', $filename;
+
+my $i = 1;
+
+my $total = 0;
+
+print "Iniciando coleta.\n";
 
 while (1){
   $curl->setopt(CURLOPT_HEADER,0);
@@ -32,16 +41,28 @@ while (1){
 
   my $retcode = $curl->perform;
 
+  # print $response_body."\n";
+
   my $decoded = decode_json($response_body);
 
   if ( $retcode == 0){
+
     my @comments = @{ $decoded->{'data'} };
+    my $qtd = @comments;
+    my $total += $qtd;
+
+    print $qtd." comentários na página $i\n";
+
     foreach my $comment ( @comments ){
-      $file_handle->print("Comentario: ".$comment->{'message'}."\n");
+      print $fh $comment->{'message'}." ";
     }
-    if ( $decoded->{'paging'}){
+    if ( $decoded->{'paging'}{'next'}){
       $url = $decoded->{'paging'}{'next'};
+      $i++;
     } else {
+      print $total." comentários coletados.\n";
+      print "Fim da coleta!\n";
+      close $fh;
       exit;
     }
   } else {
